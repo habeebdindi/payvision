45f566436070d4be56aff9274366f5f720f3d2cf
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-02-24 22:03:20 +0100

    Add backend entry point

---
+++ payvision-be/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+
+const cors = require('cors');
+const db = require('./models');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', require('./routes/auth.route'));
+
+app.get('/', (req, res) => {res.send("Welcome to payvision backend!")});
+
+app.listen(port, () => {console.log(`server running port ${port}`);});
