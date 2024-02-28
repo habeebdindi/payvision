37d4113ad75ae398e87be944e97de1fb0985a420
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-02-28 02:32:41 +0100

    Correct env variable name

--- payvision-be/config/db.config.js
+++ payvision-be/config/db.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  HOST: process.env.HOST,
+  HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASS,
   DB: process.env.DB,
