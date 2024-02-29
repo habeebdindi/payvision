17dfabe896aa81468bceb940ddd7d192bbf3c7a1
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-02-29 03:28:36 +0100

    Refactor seed file to correspond with refactored models

--- payvision-be/seed.js
+++ payvision-be/seed.js
@@ -9,47 +9,48 @@ const Tag = db.tag;
 
 
 module.exports = async function seed() {
-    try {
-	let user = await User.findOne({where: {username: 'payvision'}});
-	if (!user) {
-            user = await User.create({
-                username: "payvision",
-		fullname: "ALX Payvision",
-                email: "admin@payvision.com",
-                password: bcrypt.hashSync("PAYVISION_PASS", 5),
-		balance: faker.random.float({min: 0, max: 1000000, precision: 2}),
-		totalCredit: faker.random.float({min: 0, max: 10000000, precision: 2}),
-		totalDebit: faker.random.float({min: 0, max: 10000000, precision: 2}),
-            });
-	}
-
-	const tags = ['debit', 'credit'];
-	const tag = await Tag.create({
-	    name: tags[Math.floor(Math.random() * tags.length)]
-	});
-
-	const categories = ['food', 'clothing', 'transportation',
-			     'utilities', 'entertainment', 'miscellaneous'];
-	const category = await Category.create({
-	    name: categories[Math.floor(Math.random() * categories.length)],
-	    description: faker.lorem.paragraph(),
-	    tagId: tag.id
-	});
-
-	const methods = ['card', 'cash', 'item'];
-	const frequencies = ['daily', 'weekly', 'monthly', 'quarterly', 'yearly'];
-	const transaction = await Transaction.create({
-	    description: faker.lorem.paragraph(),
-	    paymentMethod: methods[Math.floor(Math.random() * methods.length)],
-	    date: faker.date.future(),
-	    currency: faker.finance.currencyCode(),
-	    recurred: faker.random.boolean(),
-	    frequency: frequencies[Math.floor(Math.random() * frequencies.length)],
-	    categoryId: category.id,
-	    userId: user.id
-	});
-
-    } catch (e) {
-	console.log(e);
+  try {
+    let user = await User.findOne({where: {username: 'payvision'}});
+    if (!user) {
+      user = await User.create({
+        username: "payvision",
+        fullname: "ALX Payvision",
+        email: "admin@payvision.com",
+        password: bcrypt.hashSync("PAYVISION_PASS", 5),
+        balance: faker.random.float({min: 0, max: 1000000, precision: 2}),
+        totalCredit: faker.random.float({min: 0, max: 10000000, precision: 2}),
+        totalDebit: faker.random.float({min: 0, max: 10000000, precision: 2}),
+      });
     }
+
+    const tags = ['debit', 'credit'];
+    const tag = await Tag.create({
+      name: tags[Math.floor(Math.random() * tags.length)]
+    });
+
+    const categories = ['food', 'clothing', 'transportation',
+      'utilities', 'entertainment', 'miscellaneous'];
+    const category = await Category.create({
+      name: categories[Math.floor(Math.random() * categories.length)],
+      description: faker.lorem.paragraph(),
+      tagId: tag.id
+    });
+
+    const methods = ['card', 'cash', 'item'];
+    const frequencies = ['daily', 'weekly', 'monthly', 'quarterly', 'yearly'];
+    const transaction = await Transaction.create({
+      description: faker.lorem.paragraph(),
+      paymentMethod: methods[Math.floor(Math.random() * methods.length)],
+      date: faker.date.future(),
+      currency: faker.finance.currencyCode(),
+      amount: faker.random.float({min: 100, max: 200000, precision: 2}),
+      recurred: faker.random.boolean(),
+      frequency: frequencies[Math.floor(Math.random() * frequencies.length)],
+      categoryId: category.id,
+      userId: user.id
+    });
+
+  } catch (e) {
+    console.log(e);
+  }
 }
