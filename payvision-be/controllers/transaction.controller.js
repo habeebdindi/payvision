c311fc1272febe9a459cfd586c7069bbc69551ec
HenryAnyiam <anyiamhenry7@gmail.com>
2024-03-05 01:40:28 +0100

    Add transaction order

--- payvision-be/controllers/transaction.controller.js
+++ payvision-be/controllers/transaction.controller.js
@@ -59,6 +59,7 @@ exports.getTransactions = async (req, res) => {
     const { userId } = req?.user;
     const transactions = await Transaction.findAll({
       where: { 'userId': userId },
+      order: ['date', 'DESC'],
       attributes: [
         'amount',
         'date',
